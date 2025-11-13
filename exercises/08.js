7d23617269741b20ea4a7acd8d68bf97fa7da9af
Elias Nein <eliasnein09@gmail.com>
2025-11-13 17:32:13 +0100

    8g

---
+++ exercises/08.js
@@ -0,0 +1,61 @@
+/*
+const product = {
+  name: 'basketball',
+  price: 2095,
+  ['delivery-time']: '3 days'
+}
+console.log(product.price = 2595);
+console.log(product['delivery-time']);
+*/
+/*
+function comparePrice(product1, product2) {
+
+  if (product1.price < product2.price) {
+    return product1;
+  } else {
+    return product2;
+  }
+}
+
+  const product1 = {
+    name: 'döner',
+    price: 850
+  };
+
+  const product2 = {
+    name: 'dürüm',
+    price: 750
+  };
+
+console.log(comparePrice(product1, product2));
+*/
+/*
+function isSameProduct(product1, product2) {
+  if (product1.name === product2.name && product1.price === product2.price) {
+    return true;
+  } else {
+    return false
+  }
+}
+
+const product1 = {
+  name: 'döner',
+  price: 850
+}
+
+const product2 = {
+  name: 'döner',
+  price: 850
+}
+
+console.log(isSameProduct(product1, product2));
+*/
+
+let text = 'Hold me now im six feet from the edge and im thinking maby six feet aint so far down';
+let lowerText = text.toLowerCase();
+console.log(lowerText);
+
+let repeated = lowerText.repeat(0);
+console.log(repeated);
+
+// 8g
